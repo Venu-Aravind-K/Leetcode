c3b753bd99c1362a1498eb4f63ba0a538ce6b2ea
Venu-Aravind-K <venu29aravind@gmail.com>
2025-10-21 15:58:04 +0530

    Time: 15 ms (65.85%), Space: 12.7 MB (78.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start = 0
+        end = len(s) - 1
+        while start <= end:
+            if s[start].isalnum() == False:
+                start += 1
+            elif s[end].isalnum() == False:
+                end -= 1
+            elif s[start].lower() != s[end].lower():
+                return False
+            else:
+                start += 1
+                end -= 1
+        return True
