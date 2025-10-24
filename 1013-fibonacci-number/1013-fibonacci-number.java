96e43f13a2129fc7c341042cfa4e1e9517e2bad4
Venu-Aravind-K <venu29aravind@gmail.com>
2025-10-25 00:04:16 +0530

    Time: 0 ms (100%), Space: 40.5 MB (51.66%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 1){
+            return 1;
+        }
+        if (n == 2){
+            return 1;
+        }
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        for(int i = 3; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
